fe15c7c37951bcfbe23bea78a9fc7cbe23ecbdf4
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-04-14 21:37:54 -0300

    Tipos de dados e variaveis em C#

---
+++ DevSuperiorLinguagemEmCs/TipodDeDadosEVariaveisEmCsharp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSuperiorLinguagemEmCs;
+internal class TipodDeDadosEVariaveisEmCsharp {
+
+    static void main(String[] args) {
+
+        int idade;
+        double salario, altura;
+        char genero;
+        string nome;
+
+        idade = 31;
+        salario = 4000.0;
+        altura = 1.80;
+        genero = 'M';
+        nome = "Gabriel Sales David";
+
+        Console.WriteLine(nome);
+        Console.WriteLine(genero);
+        Console.WriteLine(altura);
+        Console.WriteLine(salario);
+        Console.WriteLine(idade);
+    }
+}
