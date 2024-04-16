2177ee443bcf67a901c04e043d385cf287fb1d4f
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-04-16 17:43:22 -0300

    Problema Idade em C#

---
+++ DevSuperiorLinguagemEmCs/Problemas/ProblemaIdadeEmC#.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSuperiorLinguagemEmCs.Problemas;
+internal class ProblemaIdadeEmC_ {
+
+    static void Main(string[] args) {
+
+        CultureInfo CI = CultureInfo.InvariantCulture;
+
+        string nome01, nome02;
+        int idade01, idade02;
+        double media;
+
+        Console.WriteLine("Dados da primeira pessoa: ");
+        Console.WriteLine("Nome: ");
+        nome01 = Console.ReadLine();
+        Console.WriteLine("Idade: ");
+        idade01 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Dados da segundo pessoa: ");
+        Console.WriteLine("Nome: ");
+        nome02 = Console.ReadLine();
+        Console.WriteLine("Idade: ");
+        idade02 = int.Parse(Console.ReadLine());
+
+        media = (idade01 + idade02) / 2.0;
+
+        Console.WriteLine("A idade de " + nome01
+                                        + " e "
+                                        + nome02
+                                        + " eh de "
+                                        + media.ToString("F1", CI)
+                                        + " anos");
+    }
+}
