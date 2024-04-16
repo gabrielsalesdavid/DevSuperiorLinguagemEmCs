1c2bcf009568b95f28245f629257ebdbd3a7e00f
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-04-16 12:25:43 -0300

    Processamento de dados com casting

---
+++ DevSuperiorLinguagemEmCs/ProcessamentoDeDados/ProcessamentoDeDadosEmC#.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSuperiorLinguagemEmCs.ProcessamentoDeDados;
+class ProcessamentoDeDadosEmC_ {
+
+    static void Mian(string[] args) {
+
+        CultureInfo CI = CultureInfo.InvariantCulture;
+
+        double b1, b2, h, area;
+        b1 = 6.0;
+        b2 = 8.0;
+        h = 5.0;
+        area = (b1 + b2) / 2;
+
+        Console.WriteLine(area.ToString("F2", CI));
+    }
+}
